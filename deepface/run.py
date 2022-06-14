f7a3ce3f63b9a077881e81ca3f00fd50e7eac073
blueprintparadise <rhiray03@gmail.com>
2022-06-14 09:42:36 +0530

    Update run.py

--- deepface/run.py
+++ deepface/run.py
@@ -2,4 +2,4 @@ import DeepFace
 
 IMAGE_PATH = r"C:\Users\rhira\Documents\Face_Recognition_GAIS\Face_Recog\images"
 
-DeepFace.stream(source=0, db_path= IMAGE_PATH,model_name="VGG-Face", detector_backend="mediapipe",enable_face_analysis=False)
+DeepFace.stream(source=0, db_path= IMAGE_PATH,model_name="VGG-Face", detector_backend="mtcnn",enable_face_analysis=False)
